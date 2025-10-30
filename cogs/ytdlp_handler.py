07931653081b3a5bcf91e1b008820536fc492926
Jellycone1 <phimanshu777@gmail.com>
2025-10-31 00:28:07 +0530

    Added /disconnect, comments to all Ytdlp_handler.py's functions and renamed 'stop' command to 'clear' and it does not disconnects- simply clears the queue and stops the currently playing media

--- cogs/ytdlp_handler.py
+++ cogs/ytdlp_handler.py
@@ -8,10 +8,6 @@ from collections import deque
 from os.path import join
 
 
-SONG_QUEUES = {}
-YT_URL_REGEX = re.compile(r'^https?://')
-
-
 # General function to run yt-dlp extraction in an executor
 async def search_ytdlp_async(query, ydl_opts):
     loop = asyncio.get_running_loop()
@@ -22,9 +18,13 @@ def _extract(query, ydl_opts):
         return ydl.extract_info(query, download=False)
 
 
-class Ytdlp_handler(commands.Cog):
+class Ytdlp_handler(commands.Cog):    
     def __init__(self, bot):
         self.bot = bot
+        self.SONG_QUEUES = {}
+        self.YT_URL_REGEX = re.compile(r'^https?://')
+        self.current_playing = ''  # Make it a Dictionary to handle multiple guilds
+
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -34,6 +34,10 @@ class Ytdlp_handler(commands.Cog):
     @app_commands.command(name="play", description="Play a song or add it to the queue.")
     @app_commands.describe(song_query="Search query")
     async def play(self, interaction: discord.Interaction, song_query: str):
+        """
+            Play a song from a URL or search query and or adding it to the queue.
+            Usage: /play [song name or URL] 
+        """
         await interaction.response.defer()
 
         # Gets the voice channel that the user is currently in
@@ -59,11 +63,12 @@ class Ytdlp_handler(commands.Cog):
             "youtube_include_hls_manifest": False,
         }
 
-        if YT_URL_REGEX.match(song_query):
+        if self.YT_URL_REGEX.match(song_query):
             query = song_query
         else:
             query = "ytsearch1: " + song_query
         results = await search_ytdlp_async(query, ydl_options)
+        print(results.get('duration'))
         # print(f"Results Information:\n{results}")  # uncomment to get information about video
         
         # If a search is passed then -> "entries" exists and you can move with normal flow; else: 
@@ -80,11 +85,12 @@ class Ytdlp_handler(commands.Cog):
         audio_url = first_track["url"]
         title = first_track.get("title", "Untitled")
 
-        guild_id = str(interaction.guild_id)
-        if SONG_QUEUES.get(guild_id) is None:
-            SONG_QUEUES[guild_id] = deque()
 
-        SONG_QUEUES[guild_id].append((audio_url, title))
+        guild_id = str(interaction.guild_id)
+        if self.SONG_QUEUES.get(guild_id) is None:
+            self.SONG_QUEUES[guild_id] = deque()
+
+        self.SONG_QUEUES[guild_id].append((audio_url, title))
         if voice_client.is_playing() or voice_client.is_paused():
             await interaction.followup.send(f"Added to queue: **{title}**")
         else:
@@ -94,6 +100,10 @@ class Ytdlp_handler(commands.Cog):
 
     @app_commands.command(name="skip", description="Skips the current playing song")
     async def skip(self, interaction: discord.Interaction):
+        """
+            Skip the currently playing song.
+            Usage: /skip
+        """
         if interaction.guild.voice_client and (interaction.guild.voice_client.is_playing() or interaction.guild.voice_client.is_paused()):
             interaction.guild.voice_client.stop()
             await interaction.response.send_message("Skipped the current song.")
@@ -103,6 +113,10 @@ class Ytdlp_handler(commands.Cog):
 
     @app_commands.command(name="pause", description="Pause the currently playing song.")
     async def pause(self, interaction: discord.Interaction):
+        """
+            Pause the currently playing song.
+            Usage: /pause
+        """
         voice_client = interaction.guild.voice_client
 
         # Check if the bot is in a voice channel
@@ -120,6 +134,10 @@ class Ytdlp_handler(commands.Cog):
 
     @app_commands.command(name="resume", description="Resume the currently paused song.")
     async def resume(self, interaction: discord.Interaction):
+        """
+            Resume the currently paused song.
+            Usage: /resume
+        """
         voice_client = interaction.guild.voice_client
 
         # Check if the bot is in a voice channel
@@ -135,8 +153,12 @@ class Ytdlp_handler(commands.Cog):
         await interaction.response.send_message("Playback resumed!")
 
 
-    @app_commands.command(name="stop", description="Stop playback and clear the queue.")
-    async def stop(self, interaction: discord.Interaction):
+    @app_commands.command(name="clear", description="Stop playback and clear the queue.")
+    async def clear(self, interaction: discord.Interaction):
+        """
+            Stop playback and clear the queue.
+            Usage: /clear
+        """
         await interaction.response.defer()
         voice_client = interaction.guild.voice_client
 
@@ -147,28 +169,79 @@ class Ytdlp_handler(commands.Cog):
             # return await interaction.response.send_message("I'm not connected to any voice channel.")
 
         # Clear the guild's queue
-        guild_id_str = str(interaction.guild_id)
-        if guild_id_str in SONG_QUEUES:
-            SONG_QUEUES[guild_id_str].clear()
+        await self.clear_queue(str(interaction.guild_id))
 
         # If something is playing or paused, stop it
         if voice_client.is_playing() or voice_client.is_paused():
             voice_client.stop()
 
-        await interaction.followup.send("Stopped playback and disconnected!")
+        await interaction.followup.send("Stopped playback and Cleared Queue.")
 
         # (Optional) Disconnect from the channel
+        # await voice_client.disconnect()
+
+
+    @app_commands.command(name="disconnect", description="Disconnect bot from voice channel.")
+    async def disconnect(self, interaction: discord.Interaction):
+        """
+            Disconnect the bot from the voice channel
+            Usage: /disconnect
+        """
+        voice_client = interaction.guild.voice_client
+        user_voice = getattr(interaction.user.voice, "channel", None)
+        
+        if not user_voice:
+            await interaction.response.send_message("You are not connected to any Voice channels.")
+            return
+
+        if not voice_client:
+            await interaction.response.send_message("I am not connected to any Voice channels.")
+            return
+        
+        if user_voice != voice_client.channel:
+            await interaction.response.send_message("You are not in the same voice channel as me.")
+            return
+        
+        await self.clear_queue(str(interaction.guild_id))
+        if voice_client.is_playing() or voice_client.is_paused():
+            voice_client.stop()
+        await asyncio.sleep(1)  # Give it a moment to stop properly (for ffmpeg)
         await voice_client.disconnect()
+        await interaction.response.send_message("Disconnected Successfully!")
 
 
-    @app_commands.command(name="queue", description="Display the current song queue.")
+    @app_commands.command(name="queue", description="Check the media in queue.")
     async def queue(self, interaction: discord.Interaction):
-        pass
+        """
+            Check the current media queue.
+            Usage: /queue
+        """
+        guild_id = interaction.guild.id
+        queue = self.SONG_QUEUES.get(str(guild_id), [])
+        message = [f"\nCurrent Media: **{self.current_playing}**"]
+        # print(self.current_playing)
 
+        for index, item in enumerate(list(queue)[:10]):
+            message.append(f"{index+1} : {item[1]}")
+
+        await interaction.response.send_message("\n".join(message))
+
+
+    async def clear_queue(self, guild_id):
+        """
+            Helper function to clear the song queue for a specific guild.
+        """
+        if guild_id in self.SONG_QUEUES:
+            self.SONG_QUEUES[guild_id].clear()
+        
 
     async def play_next_song(self, voice_client, guild_id, channel):
-        if SONG_QUEUES[guild_id]:
-            audio_url, title = SONG_QUEUES[guild_id].popleft()
+        """
+            Helper function to play the next song in the queue.
+        """
+        if self.SONG_QUEUES[guild_id]:
+            audio_url, title = self.SONG_QUEUES[guild_id].popleft()
+            self.current_playing = title
 
             ffmpeg_options = {
                 "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
@@ -186,8 +259,9 @@ class Ytdlp_handler(commands.Cog):
             asyncio.create_task(channel.send(f"Now playing: **{title}**"))
 
         else:
-            await voice_client.disconnect()
-            SONG_QUEUES[guild_id] = deque()
+            # await voice_client.disconnect()
+            self.SONG_QUEUES[guild_id] = deque()
+
 
 
 async def setup(bot):
